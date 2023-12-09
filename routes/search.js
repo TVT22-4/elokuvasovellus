9e89417ec95588a8e50ef0c8110d13e8d9573d4e
Laura <127945558+lavia1@users.noreply.github.com>
2023-12-09 17:31:19 +0200

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -4,50 +4,20 @@ const upload = multer({ dest: 'upload/' });
 const axios = require('axios');
 
 
-// accessToken
 const accessToken = process.env.ACCESS_TOKEN;
 
-// Function to fetch movie genres from TMDb
-async function getMovieGenres() {
-    try {
-        const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list', {
-            params: {
-                language: 'en-US',
-                
-            },
-            headers: {
-                Authorization: 'Bearer ' + accessToken, // Replace with your actual access token
-                accept: 'application/json',
-            },
-        });
-
-        return response.data.genres;
-    } catch (error) {
-        throw new Error('Error fetching movie genres from TMDb');
-    }
-}
-
-// Function to get genre name based on ID
-function getGenreName(genreList, genreId) {
-    const genre = genreList.find(g => g.id === genreId);
-    return genre ? genre.name : 'Unknown';
-}
-
+// Endpoint for searching movies and series
 router.get('/search', upload.none(), async function (req, res) {
-    const { title, genre, type } = req.query;
+    const { title, targetId, genre, type } = req.query;
 
-    console.log('Query Parameters:', { title, genre, type });
+    console.log('Query Parameters:', { title, targetId, genre, type });
 
     try {
-        // Fetch movie genres from TMDb
-        const movieGenres = await getMovieGenres();
-
-        // Request to TMDb API to search for movies and series
+        // Request to TMDb API to search for movies
         const tmdbResponse = await axios.get('https://api.themoviedb.org/3/search/multi', {
             params: {
                 query: title,
                 page: 1,
-                with_genres: genre,
             },
             headers: {
                 Authorization: 'Bearer ' + accessToken,
@@ -60,28 +30,15 @@ router.get('/search', upload.none(), async function (req, res) {
         const content = tmdbResults.map(item => ({
             targetId: item.id,
             title: item.title || item.name,
-            genre: item.genre_ids && item.genre_ids.length > 0
-                ? item.genre_ids.map(id => getGenreName(movieGenres, id)).join(', ')
-                : 'Unknown',
             type: item.media_type === 'movie' ? 'movie' : 'series',
         }));
 
         // Filter content based on search criteria
         const results = content.filter(item =>
             (!title || item.title.toLowerCase().includes(title.toLowerCase())) &&
-            (!genre || item.genre.toLowerCase().includes(genre.toLowerCase())) &&
+            (!targetId || item.targetId === targetId) &&
             (!type || item.type.toLowerCase() === type.toLowerCase())
         );
-
-        // If both title and genre are provided but no content matches
-        if (title && genre && results.length === 0) {
-            const errorMessage = 'No content found with the provided title and genre.';
-            console.error(errorMessage);
-            return res.status(400).json({
-                status: 'error',
-                message: errorMessage,
-            });
-        }
 
         // Return the filtered results
         res.json({ status: 'success', results });
@@ -94,6 +51,9 @@ router.get('/search', upload.none(), async function (req, res) {
     }
 });
 
+
+
 module.exports = router;
+
 
 
