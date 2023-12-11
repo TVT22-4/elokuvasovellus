3d2b946ef33c57b22cc29f5ff9351fb57cf6f161
Laura <127945558+lavia1@users.noreply.github.com>
2023-12-11 11:36:12 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+<CustomUser/>
+  );
+}
+
+function CustomUser() {
+  const [moviesAndSeries, setMoviesAndSeries] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/custom_user/movies_and_series')
+      .then(response => {
+        setMoviesAndSeries(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {moviesAndSeries && (
+        <div>
+          <h2>Popular Movies</h2>
+          <ul>
+            {moviesAndSeries.popularMovies.map(movie => (
+              <li key={movie.id}>{movie.title}</li>
+            ))}
+          </ul>
+
+          <h2> Top Rated Movies</h2>
+          <ul>
+            {moviesAndSeries.topRatedMovies.map(movie => (
+            <li key={movie.id}>{movie.title}</li>
+            ))}
+          </ul>
+
+          <h2>Popular Series</h2>
+          <ul>
+            {moviesAndSeries.popularSeries.map(series => (
+              <li key={series.id}>{series.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+export default App;
