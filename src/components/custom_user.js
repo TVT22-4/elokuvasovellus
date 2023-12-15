5cccfb3a50e541fb3c992119f6e58b6bfe8c230c
Laura <127945558+lavia1@users.noreply.github.com>
2023-12-15 11:01:31 +0200

    watchlist päivitetty

--- src/components/custom_user.js
+++ src/components/custom_user.js
@@ -1,23 +1,77 @@
 import axios from 'axios';
 import { useEffect, useState } from "react";
-
+import { jwtToken } from './signals';
 
 export default function CustomUser() {
     const [moviesAndSeries, setMoviesAndSeries] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [watchList, setWatchList] = useState([]);
   
     useEffect(() => {
-      axios.get('http://localhost:3001/custom_user/movies_and_series')
-        .then(response => {
+      const fetchMoviesAndSeries = async () => {
+        try {
+          const token = jwtToken.value;
+          const config = {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          };
+  
+          const response = await axios.get('/custom_user/movies_and_series', config);
+  
+          setWatchList(response.data.watchList);
           setMoviesAndSeries(response.data);
           setLoading(false);
-        })
-        .catch(error => {
+        } catch (error) {
           setError(error);
           setLoading(false);
-        });
+        }
+      };
+  
+      fetchMoviesAndSeries();
     }, []);
+
+    //add to watchlist
+    const addToWatchList = async (targetId, postType) => {
+      try {
+        const token = jwtToken.value;
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+
+        await axios.post('custom_user/movies_and_series/watchlist', {
+          userName: token.username,
+          postType,
+          targetId,
+        },config);
+
+        setWatchList((prevWatchList) => [...(prevWatchList || []), { id: targetId, type: postType }]);
+      }catch (error){
+        console.log('Error adding to watchlist', error.message);
+      }
+    };
+
+    //delete from watchlist
+    const removeFromWatchList = async (materialID) => {
+      try {
+        const token = jwtToken.value;
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const userName = token.username;
+        await axios.delete(`/custom_user/movies_and_series/watchlist/${materialID}/${userName}`,config);
+
+        setWatchList((prevWatchList) => prevWatchList.filter(item => item.id !==materialID));
+      }catch (error){
+        console.log('Error removing from watchlist', error.message);
+      }
+    };
+
   
     return (
       <div>
@@ -25,28 +79,60 @@ export default function CustomUser() {
         {error && <p>Error: {error.message}</p>}
         {moviesAndSeries && (
           <div>
+
+            {/*Popular movies for user page*/}
             <h2>Popular Movies</h2>
             <ul>
               {moviesAndSeries.popularMovies.map(movie => (
-                <li key={movie.id}>{movie.title}</li>
+                <li key={movie.id}>
+                  <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title}/>
+                  <button onClick={() => addToWatchList(movie.id, 'movie')}>Add to watchlist</button>
+                </li>
+                
               ))}
             </ul>
   
+            {/*Top rated movies for user page*/}
             <h2> Top Rated Movies</h2>
             <ul>
               {moviesAndSeries.topRatedMovies.map(movie => (
-              <li key={movie.id}>{movie.title}</li>
+              <li key={movie.id}>
+                <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title}/>
+                <button onClick = {() => addToWatchList(movie.id,'movie')}>Add to watchlist</button>
+              </li>
+                
               ))}
             </ul>
   
+            {/*Popular series for user page*/}
             <h2>Popular Series</h2>
             <ul>
               {moviesAndSeries.popularSeries.map(series => (
-                <li key={series.id}>{series.name}</li>
+                <li key={series.id}>
+                  <img src={`https://image.tmdb.org/t/p/w300${series.poster_path}`} alt={series.name}/>
+                  <button onClick = {()=> addToWatchList(series.id, 'series')}>Add to watchlist</button>
+                </li>
               ))}
             </ul>
           </div>
         )}
+
+<h2>Watchlist</h2>
+<ul>
+  {Array.isArray(watchList) && watchList.map(item => (
+    <li key={item.id}>
+      <span>{item.type === 'movie' ? 'Movie' : 'Series'}</span>
+      <img
+        src={`https://image.tmdb.org/t/p/w300${item.type === 'movie' ? item.poster_path : (item.series && item.series.poster_path)}`}
+        alt={`Watchlist ${item.type}`}
+        style={{ width: '100px', height: '150px' }}  
+      />
+      <button onClick={() => removeFromWatchList(item.id)}>Remove from watchlist</button>
+    </li>
+  ))}
+</ul>
+
+
       </div>
     );
   }
