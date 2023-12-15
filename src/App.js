5ea5a8ebeb32f75200ee1af19992b9a9dcde2982
Laura <127945558+lavia1@users.noreply.github.com>
2023-12-15 11:03:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import CustomUser from './components/custom_user';
+import Search from './components/search';
+
 
 
 function App() {
   return (
     <BrowserRouter>
-     <Link to={'/customUser'}>User x</Link>
+     <Link to={'/customUser'}>User</Link>
+     <Search/>
       <Routes>
         <Route path = '/customUser' element = {<CustomUser/>}/>
       </Routes>
@@ -14,4 +17,3 @@ function App() {
 }
 
 export default App;
-
