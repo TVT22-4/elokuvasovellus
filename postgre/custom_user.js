3ecb18e1ca0999e4fbeec4cd6b89fb21a1ec12e1
Laura <127945558+lavia1@users.noreply.github.com>
2023-12-15 11:07:26 +0200

    Update custom_user.js

--- postgre/custom_user.js
+++ postgre/custom_user.js
@@ -2,13 +2,13 @@ const pgPool = require ('./connection');
 
 const sql = {
     INSERT_POST: 'INSERT INTO custom_user (username, posttype, targetid) VALUES ($1, $2, $3)',
-    GET_POST:'SELECT username, posttype, targetid FROM custom_user ',
-    GET_USER: 'SELECT username, posttype, targetid FROM custom_user WHERE materialid = $1',
-    DELETE_POST: 'DELETE FROM custom_user WHERE materialid=$1'
+    GET_POST:'SELECT materialid, username, posttype, targetid FROM custom_user ',
+    GET_USER: 'SELECT materialid username, posttype, targetid FROM custom_user WHERE username = $1',
+    DELETE_POST: 'DELETE FROM custom_user WHERE materialid = $1 AND username =$2'
     
 };
 
-//addPost('jappe', 'movie', '3');
+//deletePost('13');
 
 async function addPost( userName, postType, targetId){
    await pgPool.query(sql.INSERT_POST, [userName, postType, targetId]);
@@ -20,16 +20,17 @@ async function getPost(){
     return rows;
 }
 
-async function getUser(materialID) {
-    const result = await pgPool.query(sql.GET_USER, [materialID]);
+async function getUser(userName) {
+    const result = await pgPool.query(sql.GET_USER, [userName]);
     const rows = result.rows;
     return rows;
 }
 
 
-async function deletePost(materialID) {
-    await pgPool.query(sql.DELETE_POST, [materialID]);
+async function deletePost(materialID, userName) {
+    await pgPool.query(sql.DELETE_POST, [materialID, userName]);
 }
 
 module.exports = { addPost, getPost, deletePost, getUser };
+
 
