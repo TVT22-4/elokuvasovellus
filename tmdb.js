e769c2265509d6184363ec393dede9c5f4aa6027
vilmavuorma <112632790+vilmavuorma@users.noreply.github.com>
2023-12-15 20:12:56 +0200

    Update tmdb.js

--- tmdb.js
+++ tmdb.js
@@ -3,6 +3,47 @@ const axios = require('axios');
 //movies
 async function getMoviesFromTMDB(accessToken, options = {}) {
   try {
+    const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
+        params: {
+            language: 'en-US',
+            page:1,
+            ...options,
+            
+        },
+        headers: {
+            Authorization: "Bearer "+ accessToken,
+            accept: 'application/json',
+        },
+    });
+    
+    return response.data.results;
+  } catch (error) {
+    throw new Error('Error fetching movies from TMDb');
+  }
+}
+
+//series
+async function getSeriesFromTMDB(accessToken, options = {}) {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/discover/tv' , {
+      params: {
+        language: 'en-US',
+        page:1,
+        ...options,
+      },
+      headers: {
+        Authorization: "Bearer "+ accessToken,
+        accept: 'application/json',
+      },
+    });
+
+    return response.data.results;
+  }catch (error) {
+    throw new Error ('Error fetching series from TMDb');
+  }
+}
+async function getPopularMoviesFromTMDB(accessToken, options = {}) {
+  try {
     const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
         params: {
             language: 'en-US',
@@ -22,4 +63,8 @@ async function getMoviesFromTMDB(accessToken, options = {}) {
   }
 } 
 
-module.exports = {getMoviesFromTMDB};
+module.exports = {
+  getMoviesFromTMDB,
+  getSeriesFromTMDB,
+  getPopularMoviesFromTMDB,
+};
